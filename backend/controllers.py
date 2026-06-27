36181ad4c4d2f90a1f324dbea68b21094d897a44
Suraj Kumar <24f3003507@ds.study.iitm.ac.in>
2026-06-27 20:56:49 +0530

    All dashboard created + minor frontend and backend chabges

--- backend/controllers.py
+++ backend/controllers.py
@@ -1,9 +1,14 @@
 #App routes
-from flask import Blueprint, Flask,render_template,request,url_for,redirect
+from flask import Blueprint, Flask,render_template,request,url_for,redirect,flash ,session 
 from .models import *
 from datetime import datetime
 #from flask import current_app as app
 auth_bp = Blueprint('auth', __name__)
+
+ADMIN_CREDENTIALS = {
+    'username': 'admin',
+    'password': 'admin123'
+}
 
 @auth_bp.route("/")
 def home():
@@ -15,17 +20,30 @@ def login():
     if request.method=="POST":
         uname=request.form.get("username")
         pwd=request.form.get("password")
+
+        if uname == ADMIN_CREDENTIALS['username'] and pwd == ADMIN_CREDENTIALS['password']:
+                session.clear()
+                session['username'] = uname
+                session['role'] = 'admin'
+                return redirect(url_for('auth.admin_dashboard'))
+
         usr=Users.query.filter_by(username=uname,password=pwd).first()
-        if usr and usr.role==0: #Existed and admin
-            return redirect(url_for("auth.admin_dashboard",name=uname))
-        elif usr and usr.role==1: #Existed and normal user
+
+        if usr and usr.role==1: #Existed and normal user
+            session.clear()
+            session['username'] = usr.username
+            session['role'] = 'user'
             return redirect(url_for("auth.user_dashboard",name=uname,id=usr.id))
         
         elif usr and usr.role == 2:  # Staff
             if not usr.is_approved:
                 return render_template("login.html", msg="Your account is pending admin approval.")
+            session.clear()
+            session['username'] = usr.username
+            session['role'] = 'staff'
             return redirect(url_for("auth.staff_dashboard", name=usr.username, id=usr.id))
         else:
+            flash('Invalid user credentials', 'denger')
             return render_template("login.html",msg="Invalid user credentials...")
 
     return render_template("login.html",msg="")
@@ -40,29 +58,31 @@ def register():
         full_name=request.form.get("full_name")
         address=request.form.get("location")
         pin_code=request.form.get("pin_code")
-        usr=Users.query.filter_by(username=uname).first()
+        usr=Users.query.filter_by(email=email).first()
         if usr:
+            flash('Sorry, this mail already registered', 'danger')
             return render_template("register.html",msg="Sorry, this mail already registered!!!")
         new_usr=Users(username=uname,email=email,password=pwd,full_name=full_name,address=address,pin_code=pin_code)
         db.session.add(new_usr)
         db.session.commit()
+        flash('Registration successful! Login to get Started', 'success')
         return redirect(url_for('auth.login'))
     return render_template("register.html",msg="")
 
 @auth_bp.route("/admin_dashboard")
 def admin_dashboard():
-    name=request.args.get("name")
-    return render_template("admin_dashboard.html", name=name)
+    #name=request.args.get(ADMIN_CREDENTIALS['username'])
+    return render_template("admin_dashboard.html")
 
 @auth_bp.route("/user_dashboard")
 def user_dashboard():
     name=request.args.get("name")
-    return render_template("user_dashboard.html", name=name)
+    return render_template("user_dashboard.html")
 
 @auth_bp.route("/staff_dashboard")
 def staff_dashboard():
     name=request.args.get("name")
-    return render_template("staff_dashboard.html", name=name)
+    return render_template("staff_dashboard.html")
 
 
 
@@ -84,6 +104,7 @@ def register_staff():
         # Check duplicate
         usr = Users.query.filter_by(username=uname).first()
         if usr:
+            flash('Sorry, this Username is alredy taken!', 'danger')
             return render_template("register_staff.html", msg="Username already taken!")
 
         # Create Users entry
@@ -115,3 +136,10 @@ def register_staff():
 
         return redirect(url_for('auth.login'))
     return render_template("register_staff.html", msg="")
+
+@auth_bp.route('/logout')
+def logout():
+    session.pop('user', None)
+    flash('You have been logged out.', 'info')
+    #return redirect(url_for('auth.login'))
+    return render_template('login.html')
